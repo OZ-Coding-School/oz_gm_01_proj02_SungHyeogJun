fc829d4aa603a8ea57668c66df8327a0a1ddb6b0
Sung-27 <shj_27@naver.com>
2026-01-06 23:30:45 +0900

    인물넣기

---
+++ Assets/Scripts/SHJ_Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    CharacterObject[] characterObject; //애는 캐릭터 정보 받기
+    MapObject[] mapObjects;            //맵정보 받기
+    EventObject[] eventObjects;        //이벤트조건
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SHJ_Scripts/Object/CharacterObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterObject : ScriptableObject
+{
+
+    CharacterEnum characterEnum; //enum(이벤트설정 팩토리로 등록 아직 안만듬)
+    ClassSoldier classSoldier;   //해당 병종
+    string name;                 //이름
+    int lv;                      //LV
+
+
+}
